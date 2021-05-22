59acee99b41222d22e85e0326ade774136793138
张家人 <blf53517230>
2021-05-22 22:06:21 +0800

    'ceshi'

--- src/com/zl/biz/TestGit.java
+++ src/com/zl/biz/TestGit.java
@@ -3,5 +3,11 @@ package com.zl.biz;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("上传git成功");
+        System.out.println("上传gitgi");
     }
 }
+
+
+
+
+
